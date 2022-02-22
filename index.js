1e70c89170fda75087af704d1a52881033c696a4
Tim Eifler <tim.eif@protonmail.com>
2022-02-22 12:38:20 +0100

    Refactor function and add template

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
 const writeFile = require('./writeFile');
-// import fs from 'fs'
-//process object taskmanager for javascript
-//console.log(process.argv);
-/*
-  '/usr/local/Cellar/node/17.3.1/bin/node',
-  '/Users/tim/Development/hh-web-22-1/filegenerator/index.js',
-  'functionName'
-
-*/
-
-/*
-const functionName = process.argv[2]; // const kriegt den 3 Parameter zugewiesen
-
-console.log(functionName);
-
-*/
-
-//WO soll die Datei gespeichert werden und womit --
-//Math datei und 1+2=3 soll geschrieben werden
-
-//Create file with parameter which you enter and create it with empty function
-
 const inquirer = require('inquirer');
 
 inquirer
   .prompt([
     {
       name: 'functionName',
-      message: 'What is the name of your function?',
+      message: 'What the name of the function?',
     },
     {
-      name: 'faveColor',
-      message: 'What is your favorite color?',
-      default: '#008f68',
+      type: 'checkbox',
+      name: 'fileTypes',
+      message: 'Which file types should be created?',
+      choices: ['component', 'spec', 'stories'],
+      validate: answer => {
+        if (answer.length < 1) {
+          return 'You must select at least one type!';
+        }
+        return true;
+      },
     },
   ])
   .then(answers => {
-    writeFile(answers.functionName);
+    console.log(answers);
+    answers.fileTypes.forEach(type => {
+      writeFile(answers.functionName, type);
+    });
   });
--- writeFile.js
+++ writeFile.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
+const templates = require('./templates');
 
-function writeFile(name) {
-  fs.writeFileSync(
-    `./${name}.js`,
-    `function ${name}() {
+function writeFile(name, fileType) {
+  const fileName =
+    fileType === 'component' ? `./${name}.js` : `./${name}.${fileType}.js`;
 
-      
-    }`
-  );
+  const fileString = templates[fileType](name);
+  // const templateFunction = templates[fileType]
+  // const fileString = templateFunction(name)
+
+  fs.writeFileSync(fileName, fileString);
 }
 
 module.exports = writeFile;
